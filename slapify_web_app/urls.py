416a34a3738f1430a40d463def1b3800b024d08a
garrett.bengtson <garrett.bengtson@ndsu.edu>
2024-05-02 17:34:18 -0500

    Added the ability to create playlists in the base_template. Also changed some styling for the sidebar so that other content doesnt clip into it.

--- slapify_web_app/urls.py
+++ slapify_web_app/urls.py
@@ -3,5 +3,6 @@ from . import views
 
 urlpatterns = [
     path('', views.index, name='index'),
-    path('playlist/<int:pk>', views.PlaylistDetailView.as_view(), name='playlist_details')
+    path('playlist/<int:pk>', views.PlaylistDetailView.as_view(), name='playlist_details'),
+    path('create_playlist', views.CreatePlaylistView.as_view(), name='create_playlist'),
 ]
--- slapify_web_app/views.py
+++ slapify_web_app/views.py
@@ -1,9 +1,11 @@
 from typing import Any
 from django.db.models.query import QuerySet
 from django.shortcuts import render
-from .models import Playlist
+from .models import Playlist, Song
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from django.urls import reverse_lazy
 
 # Create your views here.
 def index(request):
@@ -23,3 +25,16 @@ class PlaylistDetailView(generic.DetailView):
         return(
             Playlist.objects.filter(user=self.request.user)
         )
+
+class CreatePlaylistView(LoginRequiredMixin, CreateView):
+    model = Playlist
+    fields = ['name']
+
+    # redirect the user
+    success_url = reverse_lazy('index')
+
+    # set the 'user' attriubute of Playlist to the logged in user
+    def form_valid(self, form):
+        form.instance.user = self.request.user
+        return super().form_valid(form)
+    
