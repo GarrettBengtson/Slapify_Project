fd8d1a060ec3044965c9413a9898d5253c41604d
Garrett Bengtson <garrett.bengtson@ndsu.edu>
2024-05-03 16:54:48 -0500

    Added Delete Playlist and Remove Song from Playlist functionalities.

--- slapify_web_app/urls.py
+++ slapify_web_app/urls.py
@@ -4,8 +4,10 @@ from . import views
 urlpatterns = [
     path('', views.index, name='index'),
     path('playlist/<int:pk>', views.PlaylistDetailView.as_view(), name='playlist_details'),
-    path('admin_main', views.AdminView.as_view(), name='admin_details'),
     path('create_playlist', views.CreatePlaylistView.as_view(), name='create_playlist'),
+    path('playlist/<int:pk>/delete', views.DeletePlaylistView.as_view(), name="delete_playlist"),
+    path('remove_song/<int:playlist_pk>/<int:song_pk>/', views.remove_song, name='remove_song'),
+    path('admin_main', views.AdminView.as_view(), name='admin_details'),
     path('admin_user_request/<int:pk>', views.UserRequestView.as_view(), name='user_request_details'),
     path('admin_edit', views.AdminEditView.as_view(), name='admin_edit_details'),
 ]
--- slapify_web_app/views.py
+++ slapify_web_app/views.py
@@ -1,11 +1,13 @@
 from typing import Any
 from django.db.models.query import QuerySet
-from django.shortcuts import render
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import Playlist, Song, UserRequest, AdminEdit
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views import generic
 from django.views.generic.edit import CreateView, UpdateView, DeleteView
-from django.urls import reverse_lazy
+from django.urls import reverse_lazy, reverse
+from django.http import HttpResponseRedirect
+from django.core.paginator import Paginator
 
 # Create your views here.
 def index(request):
@@ -36,10 +38,10 @@ class CreatePlaylistView(LoginRequiredMixin, CreateView):
     model = Playlist
     fields = ['name']
 
-    # redirect the user
+    # redirect the user to index
     success_url = reverse_lazy('index')
 
-    # allow the create detail page to have access to the user's playlists
+    # allow the create page to have access to the user's playlists
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['user_playlists'] = Playlist.objects.filter(user=self.request.user)
@@ -49,6 +51,38 @@ class CreatePlaylistView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super().form_valid(form)
+
+class DeletePlaylistView(LoginRequiredMixin, DeleteView):
+    model = Playlist
+
+    # redirect the user to index
+    success_url = reverse_lazy('index')
+
+    # allow the delete page to have access to the user's playlists
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['user_playlists'] = Playlist.objects.filter(user=self.request.user)
+        return context
+    
+    def form_valid(self, form):
+        try:
+            self.object.delete()
+            return HttpResponseRedirect(self.success_url)
+        except Exception as e:
+            return HttpResponseRedirect(
+                reverse("delete_playlist", kwargs={"pk": self.object.pk})
+            )
+
+def remove_song(request, playlist_pk, song_pk):
+    # Get the song and playlist objects
+    song = get_object_or_404(Song, pk=song_pk)
+    playlist = get_object_or_404(Playlist, pk=playlist_pk)
+
+    # Remove the song from the playlist
+    playlist.songs.remove(song)
+
+    # Redirect back to the playlist detail page
+    return redirect('playlist_details', pk=playlist.pk)
 
 class UserRequestView(generic.DetailView):
     """Lists User Requests"""
